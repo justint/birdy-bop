1b626b92d2e9515d4f50b77f910213fb17548cc6
Justin Tennant <justin.tennant@sjsu.edu>
2016-01-31 11:06:45 -0800

    Re-add circle_transition + animate intro dialog close

---
+++ Assets/Scripts/IntroDialog_TapOK.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using TouchScript.Gestures;
+using System;
+
+public class IntroDialog_TapOK : MonoBehaviour {
+
+	private TapGesture tapGesture;
+	public GameObject intro_dialog;
+
+	private void OnEnable()
+	{
+		tapGesture = GetComponent<TapGesture>();
+
+		tapGesture.Tapped += tappedHandler;
+
+		Debug.Log("Subscribed to tap");
+
+
+	}
+
+	private void OnDisable()
+	{
+		tapGesture.Tapped -= tappedHandler;
+	}
+
+	private void tappedHandler(object sender, EventArgs e)
+	{
+		Debug.Log("Tap registered");
+		intro_dialog.GetComponent<Animator>().Play("intro_dialog_close");
+	}
+}
